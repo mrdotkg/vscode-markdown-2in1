f64821eb30404bfd7078f81a992b923e68fa7f5c
Kr <grv.rkg@gmail.com>
2025-12-06 19:08:33 +0530

    avoid git diff view to open this editor.

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,36 @@
-import * as vscode from 'vscode';
-import { MarkdownEditorProvider } from './provider';
-import { MarkdownService } from './service';
-import { FileUtil } from './common/fileUtil';
+import * as vscode from "vscode";
+import { MarkdownEditorProvider } from "./provider";
+import { MarkdownService } from "./service";
+import { FileUtil } from "./common/fileUtil";
+import { Output } from "./common/output";
 
 export function activate(context: vscode.ExtensionContext) {
-	keepOriginDiff();
-	const viewOption = { webviewOptions: { retainContextWhenHidden: true, enableFindWidget: true } };
-	FileUtil.init(context)
-	const markdownService = new MarkdownService(context);
-	const markdownEditorProvider = new MarkdownEditorProvider(context)
-	context.subscriptions.push(
-		vscode.commands.registerCommand('vsc-markdown.switch', (uri) => { markdownService.switchEditor(uri) }),
-		vscode.commands.registerCommand('vsc-markdown.paste', () => { markdownService.loadClipboardImage() }),
-		vscode.window.registerCustomEditorProvider("vsc-markdown", markdownEditorProvider, viewOption),
-	);
+  const config = vscode.workspace.getConfiguration("workbench");
+  const configKey = "editorAssociations";
+  const editorAssociations = config.get(configKey);
+  editorAssociations["git:/**/*.md"] = "default";
+  editorAssociations["gitlens:/**/*.md"] = "default";
+  editorAssociations["git-graph:/**/*.md"] = "default";
+  config.update(configKey, editorAssociations, true);
+  const viewOption = {
+    webviewOptions: { retainContextWhenHidden: true, enableFindWidget: true },
+  };
+  FileUtil.init(context);
+  const markdownService = new MarkdownService(context);
+  const markdownEditorProvider = new MarkdownEditorProvider(context);
+  context.subscriptions.push(
+    vscode.commands.registerCommand("vsc-markdown.switch", (uri) => {
+      markdownService.switchEditor(uri);
+    }),
+    vscode.commands.registerCommand("vsc-markdown.paste", () => {
+      markdownService.loadClipboardImage();
+    }),
+    vscode.window.registerCustomEditorProvider(
+      "vsc-markdown",
+      markdownEditorProvider,
+      viewOption
+    )
+  );
 }
 
-export function deactivate() { }
-
-/**
- * Git History是生成一个临时文件, 因此这里无法控制
- */
-function keepOriginDiff() {
-	const config = vscode.workspace.getConfiguration("workbench");
-	const configKey = 'editorAssociations'
-	const editorAssociations = config.get(configKey)
-	const key = '{git,gitlens,git-graph}:/**/*.{md,csv,svg}'
-	if (editorAssociations[key]) {
-		editorAssociations[key] = undefined
-		config.update(configKey, editorAssociations, true)
-	}
-}
+export function deactivate() {}
